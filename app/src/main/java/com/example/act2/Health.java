7d5109c6157a6182fb54b7788fc68f7e3ee483d4
a20antchoand <a20antchoand@inspedralbes.cat>
2022-01-28 14:08:19 +0100

    Initial commit

---
+++ app/src/main/java/com/example/act2/Health.java
@@ -0,0 +1,16 @@
+package com.example.act2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Health extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.health);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/act2/Science.java
@@ -0,0 +1,24 @@
+package com.example.act2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class Science extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.science);
+    }
+
+
+}
